aecc15552539d7f45325204d3ad90099007cc213
Ruel Nopal <radon1284@yahoo.com>
2016-11-26 22:34:46 +0800

    hqugscvhsvxsh

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.email = auth.info.email
       # user.username = auth.info.nickname
     end
   end
